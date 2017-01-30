c94b4d0b02c51f51de9bc2600e7b0c74d2509501
Pawel Krzysko <pawelkrzysko@gmail.com>
2017-01-30 16:29:39 +0100

    add which number is bigger

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,16 @@ var a = 2,
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);	
 
 
+var a = prompt("wpisz a"),
+	b = prompt("wpisz b"),
+	value = (a*a) + (2 *a * b) - (b*b);
+console.log(value);	
+
+var whatIsBigger = value > 0 ? 'wynik dodatni' : "wynik ujemny";
+console.log(whatIsBigger);
+
+if (value = 0) {
+	console.log("Wynik jest równy zero");
+} else {
+	console.log("wynik jest różny od zera");
+}
